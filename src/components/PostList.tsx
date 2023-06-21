ef13cfbb3b4ec1ea0a4dbdf9b237c53ab61cd573
Anik Mohammad <anik7303@gmail.com>
2023-06-21 14:02:17 +0600

    add loading state to PostList

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,19 +1,24 @@
 import usePosts from "../hooks/usePosts";
+import Spinner from "./Spinner";
 
 function PostList() {
-  const { data: posts, error } = usePosts();
+  const { data: posts, error, isLoading } = usePosts();
 
   if (error) return <p className="text-danger">{error.message}</p>;
   return (
     <section className="my-3">
       <h3>Posts</h3>
       <ul className="list-group my-3">
-        {posts?.map((post) => (
-          <li key={post.id} className="list-group-item">
-            <h5 className="mb-2">{post.title}</h5>
-            <p>{post.body}</p>
-          </li>
-        ))}
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          posts?.map((post) => (
+            <li key={post.id} className="list-group-item">
+              <h5 className="mb-2">{post.title}</h5>
+              <p>{post.body}</p>
+            </li>
+          ))
+        )}
       </ul>
     </section>
   );
