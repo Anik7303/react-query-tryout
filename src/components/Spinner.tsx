71bc8338641b2a8a223b0ee5e5a5c6faeeb207fa
Anik Mohammad <anik7303@gmail.com>
2023-06-21 22:43:24 +0600

    add Spinner

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,9 @@
+const Spinner = () => (
+  <section className="d-flex justify-content-center align-items-center">
+    <div className="spinner-border text-primary" role="status">
+      <span className="visually-hidden">Loading...</span>
+    </div>
+  </section>
+);
+
+export default Spinner;
