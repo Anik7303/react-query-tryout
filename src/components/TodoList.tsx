1d368e15e9ad48534548ec9e18bf7a58c669b9c4
Anik Mohammad <anik7303@gmail.com>
2023-06-21 12:59:36 +0600

    refactor: TodoList

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -21,16 +21,19 @@ function TodoList() {
 
   if (error) return <p className="text-danger">{error}</p>;
   return (
-    <ul className="list-group my-3">
-      {todos.map((todo) => (
-        <li
-          className={`list-group-item ${todo.completed ? "disabled" : ""}`}
-          key={todo.id}
-        >
-          {todo.title}
-        </li>
-      ))}
-    </ul>
+    <section className="my-3">
+      <h3>Todos</h3>
+      <ul className="list-group my-3">
+        {todos.map((todo) => (
+          <li
+            className={`list-group-item ${todo.completed ? "disabled" : ""}`}
+            key={todo.id}
+          >
+            {todo.title}
+          </li>
+        ))}
+      </ul>
+    </section>
   );
 }
 
