28ba790ce8381226e7254f67b948145d9cc5fe61
Anik Mohammad <anik7303@gmail.com>
2023-06-21 22:34:04 +0600

    add react-query (@tanstack/react-query) & react-query-devtools (@tanstack/react-query-devtools)

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
 import App from "./App.tsx";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </React.StrictMode>
 );
