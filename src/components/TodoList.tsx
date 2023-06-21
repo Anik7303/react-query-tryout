4ffe6f861ad004937def8ebdd43f580a05bb0a8a
Anik Mohammad <anik7303@gmail.com>
2023-06-21 22:44:49 +0600

    add loading state to TodoList

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,21 +1,26 @@
 import useTodos from "../hooks/useTodos";
+import Spinner from "./Spinner";
 
 function TodoList() {
-  const { data, error } = useTodos();
+  const { data, error, isLoading } = useTodos();
 
   if (error) return <p className="text-danger">{error.message}</p>;
   return (
     <section className="my-3">
       <h3>Todos</h3>
       <ul className="list-group my-3">
-        {data?.map((todo) => (
-          <li
-            className={`list-group-item ${todo.completed ? "disabled" : ""}`}
-            key={todo.id}
-          >
-            {todo.title}
-          </li>
-        ))}
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          data?.map((todo) => (
+            <li
+              className={`list-group-item ${todo.completed ? "disabled" : ""}`}
+              key={todo.id}
+            >
+              {todo.title}
+            </li>
+          ))
+        )}
       </ul>
     </section>
   );
