ba2c1247d6425d7c5af3bebe6727051311f3df8f
Anik Mohammad <anik7303@gmail.com>
2023-06-22 00:07:20 +0600

    some react-query useful configurations

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,19 @@ import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
 import App from "./App.tsx";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 3,
+      cacheTime: 300_000, // 5m = 5 * 60 * 1000 ms
+      staleTime: 10_000, // 10s = 10 * 1000 ms // default: 0
+      refetchOnWindowFocus: false, // default: true
+      refetchOnReconnect: false, // default: true
+      refetchOnMount: false, // default: true
+      keepPreviousData: true, // default: false
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
