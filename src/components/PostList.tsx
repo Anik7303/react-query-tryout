8409a0533eec89d32eff2aec02a5ec33d127e1e0
Anik Mohammad <anik7303@gmail.com>
2023-06-22 02:23:25 +0600

    react-query: infinite queries - PostList

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,23 +1,21 @@
 import { ChangeEventHandler, useState } from "react";
 import usePosts from "../hooks/usePosts";
 import Spinner from "./Spinner";
+import React from "react";
 
 function PostList() {
   const pageSize = 5;
-  const [page, setPage] = useState<number>(1);
   const [userId, setUserId] = useState<number | undefined>(undefined);
-  const { data, error, isLoading } = usePosts({ page, pageSize, userId });
-
-  const changePageNumber = (operation: "increment" | "decrement") => {
-    if (operation === "increment") setPage((state) => state + 1);
-    else setPage((state) => (state > 1 ? state - 1 : 1));
-  };
+  const { data, error, isLoading, fetchNextPage, isFetchingNextPage } =
+    usePosts({
+      pageSize,
+      userId,
+    });
 
   const handleUserIdChange: ChangeEventHandler<HTMLSelectElement> = (event) => {
     const { value } = event.target;
     if (value) setUserId(parseInt(value));
     else setUserId(undefined);
-    setPage(1);
   };
 
   if (error) return <p className="text-danger">{error.message}</p>;
@@ -38,28 +36,25 @@ function PostList() {
         {isLoading ? (
           <Spinner />
         ) : (
-          data?.map((post) => (
-            <li key={post.id} className="list-group-item">
-              <h5 className="mb-2">{post.title}</h5>
-              <p>{post.body}</p>
-            </li>
+          data.pages.map((page, index) => (
+            <React.Fragment key={`page#${index}`}>
+              {page.map((post) => (
+                <li key={post.id} className="list-group-item">
+                  <h5 className="mb-2">{post.title}</h5>
+                  <p>{post.body}</p>
+                </li>
+              ))}
+            </React.Fragment>
           ))
         )}
       </ul>
       <div className="d-flex justify-content-center align-items-center my-3">
         <button
-          className="btn btn-primary me-2"
-          disabled={page <= 1}
-          onClick={() => changePageNumber("decrement")}
-        >
-          Previous
-        </button>
-        <button
           className="btn btn-primary"
-          onClick={() => changePageNumber("increment")}
-          disabled={userId === undefined ? page >= 10 : page >= 2}
+          onClick={() => fetchNextPage()}
+          disabled={isFetchingNextPage}
         >
-          Next
+          {isFetchingNextPage ? "Loading..." : "Load More"}
         </button>
       </div>
     </section>
--- src/hooks/usePosts.tsx
+++ src/hooks/usePosts.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 
 type Post = {
   id: number;
@@ -9,25 +9,23 @@ type Post = {
 };
 
 type PostQuery = {
-  page: number;
   pageSize: number;
   userId?: number;
 };
 
 const initialQueryData: PostQuery = {
-  page: 1,
   pageSize: 10,
   userId: undefined,
 };
 
 const usePosts = (query: PostQuery = initialQueryData) =>
-  useQuery<Post[], Error>({
+  useInfiniteQuery<Post[], Error>({
     queryKey: ["posts", query],
-    queryFn: () =>
+    queryFn: ({ pageParam = 1 }) =>
       axios
         .get<Post[]>("https://jsonplaceholder.typicode.com/posts", {
           params: {
-            _start: (query.page - 1) * query.pageSize,
+            _start: (pageParam - 1) * query.pageSize,
             _limit: query.pageSize,
             userId: query.userId,
           },
@@ -35,6 +33,14 @@ const usePosts = (query: PostQuery = initialQueryData) =>
         .then((res) => res.data),
     staleTime: 60_000, // 1 min = 1 * 60 * 1000 ms = 60_000 ms
     keepPreviousData: true, // default: false
+    getNextPageParam: (_lastPage, allPages) => {
+      // return _lastPage.length > 0 ? ln + 1 : undefined;
+      if (query.userId === undefined) {
+        return allPages.length < 10 ? allPages.length + 1 : undefined;
+      } else {
+        return allPages.length < 2 ? allPages.length + 1 : undefined;
+      }
+    },
   });
 
 export default usePosts;
