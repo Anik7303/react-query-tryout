7488be87c1bb61169ecdf65da5b302eacc0e2001
Anik Mohammad <anik7303@gmail.com>
2023-06-22 17:27:11 +0600

    react-query: paginated & parameterized queries - TodoList

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,18 +1,45 @@
+import { ChangeEventHandler, useState } from "react";
 import useTodos from "../hooks/useTodos";
 import Spinner from "./Spinner";
 
 function TodoList() {
-  const { data: todos, error, isLoading } = useTodos();
+  const pageSize = 10;
+  const [page, setPage] = useState<number>(1);
+  const [userId, setUserId] = useState<number | undefined>(undefined);
+  const { data, error, isLoading } = useTodos({ page, pageSize, userId });
+
+  const handleUserIdChange: ChangeEventHandler<HTMLSelectElement> = (event) => {
+    const { value } = event.target;
+    if (value) setUserId(parseInt(value));
+    else setUserId(undefined);
+    setPage(1);
+  };
+
+  const changePageNumber = (operation: "increment" | "decrement") => {
+    if (operation === "increment")
+      setPage((page) => (page < 10 ? page + 1 : page));
+    else setPage((page) => (page > 1 ? page - 1 : page));
+  };
 
   if (error) return <p className="text-danger">{error.message}</p>;
   return (
     <section className="my-3">
       <h3>Todos</h3>
+      <select
+        className="form-select my-3"
+        value={userId}
+        onChange={handleUserIdChange}
+      >
+        <option value="">All users</option>
+        <option value="1">User 1</option>
+        <option value="2">User 2</option>
+        <option value="3">User 3</option>
+      </select>
       <ul className="list-group my-3">
         {isLoading ? (
           <Spinner />
         ) : (
-          todos?.map((todo) => (
+          data?.map((todo) => (
             <li
               className={`list-group-item ${todo.completed ? "disabled" : ""}`}
               key={todo.id}
@@ -22,6 +49,22 @@ function TodoList() {
           ))
         )}
       </ul>
+      <section className="d-flex justify-content-center align-items-center">
+        <button
+          className="btn btn-primary me-1"
+          onClick={() => changePageNumber("decrement")}
+          disabled={page < 2}
+        >
+          Previous
+        </button>
+        <button
+          className="btn btn-primary"
+          onClick={() => changePageNumber("increment")}
+          disabled={userId === undefined ? page >= 10 : page >= 2}
+        >
+          Next
+        </button>
+      </section>
     </section>
   );
 }
--- src/hooks/useTodos.tsx
+++ src/hooks/useTodos.tsx
@@ -8,16 +8,35 @@ export type Todo = {
   completed: boolean;
 };
 
-function useTodos() {
+type TodoQuery = {
+  page: number;
+  pageSize: number;
+  userId?: number;
+};
+
+const initialQueryData = {
+  page: 1,
+  pageSize: 10,
+  userId: undefined,
+};
+
+function useTodos(query: TodoQuery = initialQueryData) {
   const fetchTodos = () =>
     axios
-      .get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
+      .get<Todo[]>("https://jsonplaceholder.typicode.com/todos", {
+        params: {
+          _start: (query.page - 1) * query.pageSize,
+          _limit: query.pageSize,
+          userId: query.userId,
+        },
+      })
       .then((res) => res.data);
 
   return useQuery<Todo[], Error>({
-    queryKey: ["todos"],
+    queryKey: ["todos", query],
     queryFn: fetchTodos,
     staleTime: 10_000, // 10s = 10 * 1000 ms = 10_000 ms
+    keepPreviousData: false,
   });
 }
 
