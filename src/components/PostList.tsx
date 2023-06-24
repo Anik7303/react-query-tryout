b4a9545b72197b3c645fa440e067d3b566c6491b
Anik Mohammad <anik7303@gmail.com>
2023-06-24 11:31:43 +0600

    react-query: paginated query - PostList

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -3,18 +3,34 @@ import usePosts from "../hooks/usePosts";
 import Spinner from "./Spinner";
 
 function PostList() {
+  const pageSize = 5;
+  const [page, setPage] = useState<number>(1);
   const [userId, setUserId] = useState<number | undefined>(undefined);
-  const { data, error, isLoading } = usePosts({ userId });
+  const { data, error, isLoading } = usePosts({ page, pageSize, userId });
+
+  const changePageNumber = (op: "increment" | "decrement"): void => {
+    if (op === "decrement") setPage((page) => (page > 1 ? page - 1 : page));
+    else if (op === "increment")
+      setPage((page) => {
+        if (userId) {
+          if (page < 2) return page + 1;
+          return page;
+        }
+        if (page < 10) return page + 1;
+        return page;
+      });
+  };
 
   if (error) return <p className="text-danger">{error.message}</p>;
   return (
-    <section className="my-3">
-      <h3>Posts</h3>
+    <section className="my-3 d-flex flex-column align-items-center">
+      <h3 className="align-self-start">Posts</h3>
       <select
         className="form-select"
         value={userId}
         onChange={({ target: { value } }) => {
           setUserId(value ? parseInt(value) : undefined);
+          setPage(1);
         }}
       >
         <option value="">All</option>
@@ -34,6 +50,22 @@ function PostList() {
           ))
         )}
       </ul>
+      <div className="d-flex justify-content-center align-items-center my-3">
+        <button
+          className="btn btn-primary me-1"
+          onClick={() => changePageNumber("decrement")}
+          disabled={page <= 1}
+        >
+          Previous
+        </button>
+        <button
+          className="btn btn-primary"
+          onClick={() => changePageNumber("increment")}
+          disabled={!userId ? page >= 10 : page >= 2}
+        >
+          Next
+        </button>
+      </div>
     </section>
   );
 }
--- src/hooks/usePosts.tsx
+++ src/hooks/usePosts.tsx
@@ -10,9 +10,11 @@ type Post = {
 
 type PostQuery = {
   userId?: number;
+  page: number;
+  pageSize: number;
 };
 
-const initialQueryData: PostQuery = { userId: undefined };
+const initialQueryData: PostQuery = { page: 1, pageSize: 10 };
 
 const usePosts = (query: PostQuery = initialQueryData) =>
   useQuery<Post[], Error>({
@@ -21,6 +23,8 @@ const usePosts = (query: PostQuery = initialQueryData) =>
       axios
         .get<Post[]>("https://jsonplaceholder.typicode.com/posts", {
           params: {
+            _start: (query.page - 1) * query.pageSize,
+            _limit: query.pageSize,
             userId: query.userId,
           },
         })
