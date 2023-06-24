1e7fd538f1f3aede36af3a4916c7b4fd3995f03a
Anik Mohammad <anik7303@gmail.com>
2023-06-24 17:30:44 +0600

    react-query: use mutation - todos

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,11 +1,34 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { FormEventHandler, useRef } from "react";
+import todoService, { Todo } from "../services/todo-service";
 
 function TodoForm() {
   const ref = useRef<HTMLInputElement>(null);
+  const queryClient = useQueryClient();
+  const addTodo = useMutation<Todo, Error, Todo>({
+    mutationFn: todoService.post,
+
+    onSuccess: (data, _variables) => {
+      // Approach 1: invalidate cache
+      // queryClient.invalidateQueries({ queryKey: ["todos"] }); // This doesn't work with jsonplaceholder api
+
+      // Approach 2: manuall update cache
+      queryClient.setQueryData<Todo[]>(["todos"], (todos = []) => [
+        data,
+        ...todos,
+      ]);
+    },
+  });
+
   const handleSubmit: FormEventHandler<HTMLFormElement> = (event) => {
     event.preventDefault();
     if (ref.current && ref.current.value) {
-      console.log(ref.current.value);
+      addTodo.mutate({
+        id: 0,
+        title: ref.current.value,
+        completed: false,
+        userId: 1,
+      });
       ref.current.value = "";
     }
   };
--- src/hooks/useTodos.ts
+++ src/hooks/useTodos.ts
@@ -1,29 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import todoService, { Todo } from "../services/todo-service";
 
-type TodoQuery = {
-  page: number;
-  pageSize: number;
-  userId?: number;
-};
-
-const initialQueryData = {
-  page: 1,
-  pageSize: 10,
-  userId: undefined,
-};
-
-const useTodos = (query: TodoQuery = initialQueryData) =>
+const useTodos = () =>
   useQuery<Todo[], Error>({
-    queryKey: ["todos", query],
-    queryFn: () =>
-      todoService.get({
-        params: {
-          _start: (query.page - 1) * query.pageSize,
-          _limit: query.pageSize,
-          userId: query.userId,
-        },
-      }),
+    queryKey: ["todos"],
+    queryFn: todoService.get,
     staleTime: 10_000, // 10s = 10 * 1000 ms = 10_000 ms
     keepPreviousData: false,
   });
