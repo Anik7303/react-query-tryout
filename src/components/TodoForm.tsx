1c444f08c138735f57b488760e921a8a62829f58
Anik Mohammad <anik7303@gmail.com>
2023-06-24 13:02:57 +0600

    react-query: mutation - todos

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,15 +1,39 @@
 import { FormEventHandler, useRef } from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+import { Todo } from "../hooks/useTodos";
 
-type Props = {
-  onAdd: (text: string) => void;
-};
+function TodoForm() {
+  const queryClient = useQueryClient();
+  const addTodo = useMutation<Todo, Error, Todo>({
+    mutationFn: (todo) =>
+      axios
+        .post<Todo>("https://jsonplaceholder.typicode.com/todos", todo)
+        .then((res) => res.data),
 
-function TodoForm({ onAdd }: Props) {
+    onSuccess: (savedTodo, _newTodo) => {
+      // Approach 1: invalidate cache
+      // queryClient.invalidateQueries({queryKey: ['todos']}); // This doesn't work with jsonplaceholder api
+
+      // Approach 2: manually update cache
+      queryClient.setQueryData<Todo[]>(["todos"], (todos = []) => [
+        savedTodo,
+        ...todos,
+      ]);
+    },
+  });
+
   const ref = useRef<HTMLInputElement>(null);
+
   const handleSubmit: FormEventHandler<HTMLFormElement> = (event) => {
     event.preventDefault();
     if (ref.current && ref.current.value) {
-      onAdd(ref.current.value);
+      addTodo.mutate({
+        id: 0,
+        title: ref.current.value,
+        completed: false,
+        userId: 1,
+      });
       ref.current.value = "";
     }
   };
--- src/hooks/useTodos.tsx
+++ src/hooks/useTodos.tsx
@@ -1,35 +1,21 @@
-import { useInfiniteQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
-type Todo = {
+export type Todo = {
   id: number;
   title: string;
   userId: number;
   completed: boolean;
 };
 
-type TodoQuery = {
-  pageSize: number;
-};
-
-const initialQueryData: TodoQuery = { pageSize: 10 };
-
-const useTodos = (query: TodoQuery = initialQueryData) =>
-  useInfiniteQuery<Todo[], Error>({
+const useTodos = () =>
+  useQuery<Todo[], Error>({
     queryKey: ["todos"],
-    queryFn: ({ pageParam = 1 }) =>
+    queryFn: () =>
       axios
-        .get<Todo[]>("https://jsonplaceholder.typicode.com/todos", {
-          params: {
-            _start: (pageParam - 1) * query.pageSize,
-            _limit: query.pageSize,
-          },
-        })
+        .get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
         .then((res) => res.data),
     staleTime: 10_000, // 10s = 10 * 1000 ms
-    getNextPageParam: (lastPage = [], allPages) => {
-      return lastPage.length > 0 ? allPages.length + 1 : undefined;
-    },
   });
 
 export default useTodos;
