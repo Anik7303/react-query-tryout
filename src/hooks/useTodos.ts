1a2e05aef38a96313ff98332c001ae3d593c5f9d
Anik Mohammad <anik7303@gmail.com>
2023-06-24 17:02:12 +0600

    create reusable http service - todos

--- src/hooks/useTodos.ts
+++ src/hooks/useTodos.ts
@@ -1,12 +1,5 @@
-import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
-
-export type Todo = {
-  id: number;
-  title: string;
-  userId: number;
-  completed: boolean;
-};
+import todoService, { Todo } from "../services/todo-service";
 
 type TodoQuery = {
   page: number;
@@ -20,24 +13,19 @@ const initialQueryData = {
   userId: undefined,
 };
 
-function useTodos(query: TodoQuery = initialQueryData) {
-  const fetchTodos = () =>
-    axios
-      .get<Todo[]>("https://jsonplaceholder.typicode.com/todos", {
+const useTodos = (query: TodoQuery = initialQueryData) =>
+  useQuery<Todo[], Error>({
+    queryKey: ["todos", query],
+    queryFn: () =>
+      todoService.get({
         params: {
           _start: (query.page - 1) * query.pageSize,
           _limit: query.pageSize,
           userId: query.userId,
         },
-      })
-      .then((res) => res.data);
-
-  return useQuery<Todo[], Error>({
-    queryKey: ["todos", query],
-    queryFn: fetchTodos,
+      }),
     staleTime: 10_000, // 10s = 10 * 1000 ms = 10_000 ms
     keepPreviousData: false,
   });
-}
 
 export default useTodos;
